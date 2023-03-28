39e048251ec40ffa86522ac48bad42850ab9f6aa
halfblood <arashyadegari0402@gmail.com>
2023-03-28 22:04:15 +0330

    initial commit

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            <p>
+                here is a simple code snapshot from front/src/App.js
+            </p>
+        </div>
+    );
+}
+
+export default App;
