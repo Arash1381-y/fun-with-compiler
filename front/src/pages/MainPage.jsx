623ef957b877b7c6d08992884a25288735462e00
halfblood <arashyadegari0402@gmail.com>
2023-03-31 13:31:29 +0330

    add hero section and projects to main page

---
+++ front/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "../components/mainPageComponents/HeroSection";
+import ProjectParser from "../components/mainPageComponents/ProjectParser";
+
+
+const MainPage = () => {
+    return (
+        <>
+            <HeroSection/>
+            <ProjectParser/>
+        </>
+    )
+}
+
+export default MainPage;
